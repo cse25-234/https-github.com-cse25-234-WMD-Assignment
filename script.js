6bfce7258651f867f2dada32b31a3d77e166e74c
kala <cse25-234@thuto.bac.ac.bw>
2026-05-25 19:23:25 +0200

    added script

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// ===============================
-// WELCOME MESSAGE
-// ===============================
+
 
 window.onload = function () {
     console.log("Website Loaded Successfully");
 };
+//FEEDBACK FORM VALIDATION//
 
-// ===============================
-// FEEDBACK FORM VALIDATION
-// ===============================
 
 function validateFeedback() {
 
@@ -25,10 +21,72 @@ function validateFeedback() {
     return true;
 }
 
-// ===============================
-// BUTTON MESSAGE
-// ===============================
+// BUTTON MESSAGE//
+
 
 function showMessage() {
     alert("Thank you for visiting DTEF Website!");
+}
+
+// RE-SPONSORSHIP FORM VALIDATION//
+
+
+function validateResponsorForm() {
+
+    let name = document.getElementById("responsorName").value;
+    let email = document.getElementById("responsorEmail").value;
+    let reason = document.getElementById("reason").value;
+
+    if (name === "" || email === "" || reason === "") {
+
+        alert("Please complete all required fields.");
+
+        return false;
+    }
+
+    alert("Application submitted successfully!");
+
+    return true;
+}
+
+// TOP ACHIEVERS FORM VALIDATION//
+
+
+function validateTopAchieversForm() {
+
+    let name = document.getElementById("topName").value;
+    let email = document.getElementById("topEmail").value;
+    let results = document.getElementById("results").value;
+
+    if (name === "" || email === "" || results === "") {
+
+        alert("Please complete all required fields.");
+
+        return false;
+    }
+
+    alert("Top Achievers application submitted successfully!");
+
+    return true;
+}
+
+// LOCAL PLACEMENT FORM VALIDATION//
+
+
+function validateLocalPlacementForm() {
+
+    let name = document.getElementById("localName").value;
+    let email = document.getElementById("localEmail").value;
+    let programme = document.getElementById("programme").value;
+
+    if (name === "" || email === "" || programme === "") {
+
+        alert("Please complete all required fields.");
+
+        return false;
+    }
+
+    alert("Local Placement application submitted successfully!");
+
+    return true;
 }
